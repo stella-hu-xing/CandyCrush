90cd19784b7c476988ff07c6852bd19f23506102
Xing Hu <xinghu0164@gmail.com>
2017-03-09 11:16:51 +1100

    add candy models in MVC pattern

---
+++ CandyCrushCopy/CandyCrushCopy/Candy.swift
@@ -0,0 +1,9 @@
+//
+//  Candy.swift
+//  CandyCrushCopy
+//
+//  Created by Xing Hu on 8/3/17.
+//  Copyright © 2017 Xing Hu. All rights reserved.
+//
+
+import Foundation
---
+++ CandyCrushCopy/CandyCrushCopy/Level.swift
@@ -0,0 +1,9 @@
+//
+//  Level.swift
+//  CandyCrushCopy
+//
+//  Created by Xing Hu on 8/3/17.
+//  Copyright © 2017 Xing Hu. All rights reserved.
+//
+
+import Foundation
